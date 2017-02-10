08ecc1b7d33dd5f4153e80d2a6780ef3f05abff5
gralpli <ranuberger@gmx.net>
2017-02-10 05:56:06 +0100

    try fixing 'bad instruction' errors

--- src/libcompiler_builtins/build.rs
+++ src/libcompiler_builtins/build.rs
@@ -99,6 +99,8 @@ fn main() {
         cfg.define("__BYTE_ORDER__", Some("2"));
         cfg.define("__ORDER_BIG_ENDIAN__", Some("1"));
         cfg.define("__ORDER_LITTLE_ENDIAN__", Some("2"));
+
+        cfg.define("-mcpu=arm922t");
     }
 
     let mut sources = Sources::new();
